18e582e83770bb003f719601e1122185abb52d89
Rajeenthan05 <rajeenthan0005@hotmail.com>
2024-11-04 16:43:43 +0530

    add repo,prop

---
+++ backend/src/main/java/com/Elysian_Escapade/Elysian_Escapade_Hotel/Elysian_Escapade_HotelApplication.java
@@ -0,0 +1,13 @@
+package com.Elysian_Escapade.Elysian_Escapade_Hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elysian_Escapade_HotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Elysian_Escapade_HotelApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/Elysian_Escapade/Elysian_Escapade_Hotel/Elysian_EscapadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Elysian_Escapade.Elysian_Escapade_Hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Elysian_EscapadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
