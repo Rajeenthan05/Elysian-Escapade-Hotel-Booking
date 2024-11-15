789115acc010b58ad401d325b7cf3efd19448eaf
Rajeenthan Rajaratnam <163402600+Rajeenthan05@users.noreply.github.com>
2024-11-15 14:51:16 +0530

    Update BookingRepository.java

--- backend/src/main/java/com/Elysian_Escapade/Elysian_Escapade_Hotel/repo/BookingRepository.java
+++ backend/src/main/java/com/Elysian_Escapade/Elysian_Escapade_Hotel/repo/BookingRepository.java
@@ -4,7 +4,7 @@ import com.Elysian_Escapade.Elysian_Escapade_Hotel.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
+//extend
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
