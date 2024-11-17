dc2543ae3ce94ea73f258f94ee75482227168c9d
Rajeenthan Rajaratnam <163402600+Rajeenthan05@users.noreply.github.com>
2024-11-17 08:59:45 +0530

    Update BookingContrller.java

--- backend/src/main/java/com/Elysian_Escapade/Elysian_Escapade_Hotel/controller/BookingContrller.java
+++ backend/src/main/java/com/Elysian_Escapade/Elysian_Escapade_Hotel/controller/BookingContrller.java
@@ -1,3 +1,50 @@
-public class BookingContrller {
-    
+package com.Elysian_Escapade.Elysian_Escapade_Hotel.controller;
+
+import com.Elysian_Escapade.Elysian_Escapade_Hotel.dto.Response;
+import com.Elysian_Escapade.Elysian_Escapade_Hotel.entity.Booking;
+import com.Elysian_Escapade.Elysian_Escapade_Hotel.service.interfac.IBookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bookings")
+
+public class BookingController {
+
+    @Autowired
+    private IBookingService bookingService;
+
+    @PostMapping("/book-room/{roomId}/{userId}")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
+    public ResponseEntity<Response> saveBookings(@PathVariable Long roomId,
+                                                 @PathVariable Long userId,
+                                                 @RequestBody Booking bookingRequest) {
+
+        Response response = bookingService.saveBooking(roomId, userId, bookingRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllBookings() {
+        Response response = bookingService.getAllBookings();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/get-by-confirmation-code/{confirmationCode}")
+    public ResponseEntity<Response> getBookingByConfirmationCode(@PathVariable String confirmationCode) {
+        Response response = bookingService.findBookingByConfirmationCode(confirmationCode);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @DeleteMapping("/cancel/{bookingId}")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
+    public ResponseEntity<Response> cancelBooking(@PathVariable Long bookingId) {
+        Response response = bookingService.cancelBooking(bookingId);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
 }
