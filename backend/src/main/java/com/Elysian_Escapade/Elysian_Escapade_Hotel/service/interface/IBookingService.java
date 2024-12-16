d8648c4b60ad7b202f36cb0bc8ef6320972b9cd0
Rajeenthan Rajaratnam <163402600+Rajeenthan05@users.noreply.github.com>
2024-12-16 09:43:20 +0530

    Update IBookingService.java

--- backend/src/main/java/com/Elysian_Escapade/Elysian_Escapade_Hotel/service/interface/IBookingService.java
+++ backend/src/main/java/com/Elysian_Escapade/Elysian_Escapade_Hotel/service/interface/IBookingService.java
@@ -43,25 +43,6 @@ public interface IRoomService {
     Response getAllAvailableRooms();
 }
 
-package com.Elysian_Escapade.Elysian_Escapade_Hotel.service.interface;
 
-import com.Elysian_Escapade.Elysian_Escapade_Hotel.dto.LoginRequest;
-import com.Elysian_Escapade.Elysian_Escapade_Hotel.dto.Response;
-import com.Elysian_Escapade.Elysian_Escapade_Hotel.entity.User;
-
-public interface IUserService {
-    Response register(User user);
-
-    Response login(LoginRequest loginRequest);
-
-    Response getAllUsers();
-
-    Response getUserBookingHistory(String userId);
-
-    Response deleteUser(String userId);
-
-    Response getUserById(String userId);
-
-    Response getMyInfo(String email);
 
 }
